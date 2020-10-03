de0fcbe745e243f334e090153153150ed25fd305
yusiang <10656010@ntub.edu.tw>
2020-10-03 23:50:43 +0800

    新增 - 更換密碼

--- user/user.py
+++ user/user.py
@@ -12,17 +12,51 @@ def login():
     account = content['account']
     password = content["password"]
     data=userModel.login(account,password)
-    # if(data)!=None: 
-    #     message="登入成功"
+    result={"sucess":False,"data":data}
+    if len(data)==1:
+        result["message"]="登入成功"
+        result["sucess"]=True
+    elif len(data)==0:
+        result["message"]="登入失敗"
+    else:
+        result["message"]="登入異常"
     
-    return Response(json.dumps(data,cls=MyEncoder), mimetype='application/json')
+    return Response(json.dumps(result,cls=MyEncoder), mimetype='application/json')
 
 @userProfile.route("/sign", methods=["POST"])
 def sign():
-    account = request.values.get("account")
-    password = request.values.get("password")
-    age = request.values.get("age")
-    sex = request.values.get("sex")
+    content=request.json
+    account = content['account']
+    password = content["password"]
+    age = content["age"]
+    sex = content["sex"]
     return("enter sign")
+
+
+@userProfile.route("/password",methods=["PUT"])
+def edit():
+    content=request.json
+    account=content["account"]
+    oldPassword=content["oldpassword"]
+    password=content["password"]
+    passwordConfire=content["passwordConfire"]
+    result={"success":False,"message":""}
+    oldPasswordFromDB=userModel.findPasswordByAccount(account)
+    if(oldPasswordFromDB):
+        if(oldPasswordFromDB!=oldPassword):
+            result["message"]+="輸入舊密碼錯誤\n"
+        if(password!=passwordConfire):
+            result["message"]+="密碼和確認密碼不同\n"
+        if(result["message"]):
+            data=userModel.changePassword(account,password)            
+            result["message"]="更換密碼"
+            result["data"]=data
+    else:
+        result["message"]="帳號不存在"    
+    return Response(json.dumps(result,cls=MyEncoder), mimetype='application/json')
+    
+    
+
+
 
 
