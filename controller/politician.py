a33b61bab1df933a44fa5a7590896f0b2bc5bc73
YUZHE <10656023@ntub.edu.tw>
2020-10-16 16:18:52 +0800

    新增 - 政治人物編輯

--- controller/politician.py
+++ controller/politician.py
@@ -18,3 +18,19 @@ def find():
             data[i] = content[i]
     data = politicianModel.find(data)    
     return Response(json.dumps(data["data"], cls=MyEncoder), mimetype='application/json')
+
+@politicianProfile.route("/", methods=["PATCH"])
+def changeProfile():
+    content = request.json
+    account = content["id"]
+    cond = [ "term", "sex", "partyid", "areaid", "positionid"]
+    data = {}
+    for i in cond:
+        if(i in content.keys()):
+            data[i] = content[i]
+    data = politicianModel.changePolitician(data, id)
+    result = {"success": False, "message": "修改異常", "data": data}
+    if(data["success"]):
+        result["success":True]
+        result["message"]="修改成功"
+    return Response(json.dumps(result, cls=MyEncoder), mimetype='application/json')
