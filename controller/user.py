4d01f6afdcf8cadeca37bdf696d1589d1621ff4e
sodiffi <10656010@ntub.edu.tw>
2020-11-23 17:45:33 +0800

    修正 - 登入判斷

--- controller/user.py
+++ controller/user.py
@@ -14,16 +14,23 @@ def login():
     password = content["password"]
     data = userModel.login(account, password)
     result = {"sucess": False, "data": data}
-    if(data["success"]):
-        if len(data) == 1:
+    # if(data["success"]):
+    #     if len(data) == 1:
+    #         result["message"] = "登入成功"
+    #         result["sucess"] = True
+    #     elif len(data) == 0:
+    #         result["message"] = "登入失敗"
+    #     else:
+    #         result["message"] = "登入異常"
+    # else:
+    #     result["message"] = "登入異常"
+    if len(data) == 1:
             result["message"] = "登入成功"
             result["sucess"] = True
-        elif len(data) == 0:
+    elif len(data) == 0:
             result["message"] = "登入失敗"
-        else:
-            result["message"] = "登入異常"
     else:
-        result["message"] = "登入異常"
+            result["message"] = "登入異常"
 
     return Response(json.dumps(result, cls=MyEncoder), mimetype='application/json')
 
