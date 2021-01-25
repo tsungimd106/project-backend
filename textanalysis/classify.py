f17632f42d06cb196a90d4766092ba738994b3e9
chihyu <10656026@ntub.edu.tw>
2021-01-25 22:37:13 +0800

    修改-政見分類

--- textanalysis/classify.py
+++ textanalysis/classify.py
@@ -1,4 +1,6 @@
 # coding:utf-8
+# 餘弦相似度
+# 完全相似 1.0
 
 import xiangshi as xs
 f = open("C:\\Users\\chihyu\\Desktop\\shcool\\project\\code\\textanalysis\\titles.txt",
@@ -7,11 +9,9 @@ f = f.readlines()
 Input1 = ['長照貼心 走入社區：廣佈各級長照據點，擴增服務項目，落實銀髮族照護。']
 
 for j in range(len(f)):
-    print('第', j+1, '個元素:', f[j], type(f[j]))
+    print('第', j+1, '個元素:', f[j])
     temp=[f[j]]
     result = xs.cossim(temp, Input1)
     print(result)   
 
 
-# 餘弦相似度0.190692517849
-# 完全相似 1.0
