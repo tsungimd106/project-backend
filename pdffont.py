e990875f445284f4c9d9e9ea4413cb3c973ab430
chihyu <10656026@ntub.edu.tw>
2021-06-04 15:10:48 +0800

    修改-提案分類詞性篩選

--- pdffont.py
+++ pdffont.py
@@ -1,8 +1,14 @@
+# -*-coding:utf8-*-
+from operator import index, indexOf
+from typing import Text
 from model.db import DB
 import pandas as pd
 import pdfplumber
 import requests
 from io import BytesIO
+import jieba
+import jieba.analyse
+from jieba.analyse import extract_tags
 
 # 從資料庫裡抓取pdf連結
 
@@ -13,7 +19,11 @@ content = findPdfurl()
 print(len(content["data"]))
 
 pdfdb = findPdfurl()
-
+#讀取成文字後存回資料庫
+def returnPdfcontent(content):
+    sqlstr = "insert into proposal (content) VALUES (%s)" % (
+        content)
+    return DB.execution(DB.create, sqlstr)
 
 for j in pdfdb["data"]:
     rq = requests.get(j["pdfUrl"])
@@ -22,12 +32,29 @@ for j in pdfdb["data"]:
         #print(i)
         page = pdf.pages[1]
         text = page.extract_text()
+        print(text)
+
+    tags = jieba.analyse.extract_tags(text, topK=5, withWeight=True,allowPOS=('n','a','an','nr','ns','nt','nz'))
+    stopwords = ['stopwords.txt']
+    break_words=[]
+    for j in text:
+        break_words.append(j)
+    for word in open ('stopwords.txt','r',encoding="utf-8",errors='ignore'):
+        stopwords.append(word.strip())
+    del_stopwords=[]
+    for k in break_words:
+        if k not in stopwords:
+            del_stopwords.append(k)
+    for sentence in text:
+        seg_list = jieba.lcut(sentence)
+    #seg_list = jieba.lcut(sentence)    
+    #print(seg_list)
+    for tag in tags:
+        print('word:', tag[0], 'tf-idf:', tag[1])
+
+
+        #returnPdfcontent(text)
     #讀取文字
    #text=p0.extract_text()
-    print(text)
+    #print (text)
 
-
-def returnPdfcontent(content):
-    sqlstr = "insert into proposal (content) VALUES (%s)" % (
-        content)
-    return DB.execution(DB.create, sqlstr)
