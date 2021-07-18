13ff793b7e4880b6dd8af242df847ebb32f31509
Chihyu <10656026@ntub.edu.tw>
2021-07-18 22:02:20 +0800

    調整 - 政見精準度debug

--- testtfidf.py
+++ testtfidf.py
@@ -31,14 +31,17 @@ policy = findPolicy()
 category = findCategory()
 
 
-
 # 分類與政見逐條比對
 print(category)
+print('==============')
+for cate in category["data"]:
+    print(cate["name"])
 for j in category["data"]:
-    m = [j["name"]]
+    m = j["name"]
+    # print()
     #print('第1個元素:', j)
     for i in policy["data"]:
-        n = [i["content"].decode(encoding='utf-8', errors='ignore')]  # 政見
+        n = i["content"].decode(encoding='utf-8', errors='ignore')  # 政見
         s = i["content"]
         keywords = jieba.analyse.extract_tags(n, topK=20, withWeight=True, allowPOS=('n', 'nr', 'ns'))
         print(keywords)
