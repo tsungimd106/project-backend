d20133faf7bf20bc49bbd0ae7571a8595cb476f8
YUZHE <10656023@ntub.edu.tw>
2021-07-30 00:08:14 +0800

    lline bot 政治人物

--- model/line.py
+++ model/line.py
@@ -1,27 +1,37 @@
 import json
-from model import (politicianModel,userModel)
+from model import (politicianModel, userModel)
 from app import MyEncoder
 import json
 from linebot.models import (
-    TextSendMessage, ImageSendMessage, StickerSendMessage, QuickReply, QuickReplyButton, MessageAction, URIAction)
+     URITemplateAction,TextSendMessage, ImageSendMessage, StickerSendMessage, QuickReply, QuickReplyButton, MessageAction, URIAction,ButtonsTemplate)
 
 
 class lineModule:
 
     @staticmethod
     def handle_messenge(event):
-        print(event)       
+        print(event)
         msg = event.message.text
-        if(msg == "我要area"):
-            return TextSendMessage(text=msg)
-        elif(msg == "個人檔案"):         
-            webUserid=userModel.getUserIdByLine(event.source.user_id )
-            return TextSendMessage(text='請選擇以下服務',
+        if(msg == "我要查詢政治人物"):
+            return TextSendMessage(text="輸入政治人物名稱")
+        elif(msg == "蔣萬安"):
+            return TextSendMessage(text=msg,
+                                   quick_reply=QuickReply(items=[
+                                       QuickReplyButton(action=URIAction(
+                                           label='點我進網站',
+                                           uri='https://taipei.app' 
+                                       ))
+                                   ]))
+        elif(msg == "個人檔案"):
+            webUserid = userModel.getUserIdByLine(event.source.user_id)
+            return TextSendMessage(text=msg,
                                    quick_reply=QuickReply(items=[
                                        QuickReplyButton(action=URIAction(
                                            label='個人檔案',
-                                           uri='https://taipei.app/redirect/%s/user' %webUserid
+                                           uri='https://taipei.app/redirect/%s/user' % webUserid
                                        ))
                                    ]))
         else:
             return TextSendMessage(text=msg)
+
+
