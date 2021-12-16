dc43f888c265c76321c7ddbd076a38560ce61d1f
sodiffi <10656010@ntub.edu.tw>
2021-12-16 10:48:14 +0800

    修正 - 增加提案內文資訊，留言身分及待觀察帳號

--- model/proposalModel.py
+++ model/proposalModel.py
@@ -67,23 +67,39 @@ def vote(userid, sp_id, proposal_id):
 
 def msgList(proposal_id, user_id):
     sqlstr = [
-        {"sql": f"select m.*,u.name from message as m  join user as u on m.user_id=u.id where proposal_id=\"{proposal_id}\"", "name": "msg"},
+        {"sql": "select * from rule", "name": "rule"},      
         {"sql":
          " ".join([
-             "select p.id,p.title,p.pdfUrl ,s.status,f.name as f_name ,c.name as c_name from proposal as p ",
-             " left join proposer as er on er.proposal_id=p.id ",
-             " left join politician as polit on polit.id=er.politician_id ",
-             " left join figure as f on polit.figure_id=f.id ",
-             " left join status as s on p.status_id=s.id ",
-             " left join proposal_category as pc on p.id=pc.propsoal_id ",
-             " left join category as c on pc.category_id=c.id "
-             f" where   p.id=\"{proposal_id}\" "
+             "select m.*,u.name , not isnull(r.user_id) as 'risk' ,not isnull(pa.user_id) as 'isFigure' from message as m ",
+             " join user as u on m.user_id=u.id ",
+             " left join risk as r on u.id=r.user_id ",
+             " left join politician_account as pa on u.id=pa.user_id ",
+             f"where proposal_id=\"{proposal_id}\""
 
          ]),
-            "name": "detail"},
+            "name": "msg"
+         },
         {
-            "sql": f"select * from favorite where user_id=\"{user_id}\" and proposal_id=\"{proposal_id}\"",            "name": "heart"},
-        {"sql": "select * from rule", "name": "rule"},
+            "sql":
+            " ".join([
+                "select p.id,p.title,p.pdfUrl ,s.status,f.name as f_name ,c.name as c_name from proposal as p ",
+                " left join proposer as er on er.proposal_id=p.id ",
+                " left join politician as polit on polit.id=er.politician_id ",
+                " left join figure as f on polit.figure_id=f.id ",
+                " left join status as s on p.status_id=s.id ",
+                " left join proposal_category as pc on p.id=pc.propsoal_id ",
+                " left join category as c on pc.category_id=c.id "
+                f" where   p.id=\"{proposal_id}\" "
+
+            ]),
+            "name": "detail"
+        },
+        {
+            "sql":
+            f"select * from favorite where user_id=\"{user_id}\" and proposal_id=\"{proposal_id}\"",
+            "name": "heart"
+        },
+
         {
             "sql": f"SELECT * FROM db.proposal_vote where id=\"{proposal_id}\"",
             "name": "vote"
